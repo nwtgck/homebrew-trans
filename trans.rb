0cf1e8f1f048d3d5599dce6f15f9812cd37167f4
Ryo Ota <nwtgck@gmail.com>
2018-06-14 16:17:34 +0900

    [change] Update to 0.3.0

--- trans.rb
+++ trans.rb
@@ -1,10 +1,10 @@
-TRANS_VERSION = "0.2.0"
+TRANS_VERSION = "0.3.0"
 APP_FILE_NAME = "trans_macOS_#{TRANS_VERSION}"
 
 class Trans < Formula
   desc "CLI tool for Trans"
   url "https://github.com/nwtgck/trans-cli-go/releases/download/v#{TRANS_VERSION}/#{APP_FILE_NAME}"
-  sha256 "e4de41b7e116b586fecd2de25e7b721ba1cfca460f9c320872d31c69c8883e2f"
+  sha256 "0953db65cf724905ddb38e7e4138f1cfbe16e9c00f78224d746ff0c4080ed268"
 
   def install
     system("mv #{APP_FILE_NAME} trans")
